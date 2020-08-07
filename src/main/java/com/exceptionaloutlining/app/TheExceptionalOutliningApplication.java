71dc7a0c19bbaa72c55fc1cea5918e42881fbae2
ReisHaleem <Reis.Haleem@target.com>
2020-08-07 14:29:42 -0400

    add spring boot proj

---
+++ src/main/java/com/exceptionaloutlining/app/TheExceptionalOutliningApplication.java
@@ -0,0 +1,13 @@
+package com.exceptionaloutlining.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheExceptionalOutliningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheExceptionalOutliningApplication.class, args);
+	}
+
+}
